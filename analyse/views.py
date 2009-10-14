ab9e0461879cb20ff68c77309efc73b1f0626ae9
Hu Kai <iamkaihu@gmail.com>
2009-10-15 07:55:06 +0800

    #1 finished the story 1, build can be displayed on the web page.

--- analyse/views.py
+++ analyse/views.py
@@ -1,10 +1,14 @@
 # Create your views here.
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from analyse.models import *
+
 
 def index(request):
-    t = loader.get_template('analyse/index.html')
-    c = Context({
+    builds = BuildFactory.create_builds();
+    context = Context({
+        "total" : len(Build.objects.all())
     })
-    return HttpResponse(t.render(c))
+    return render_to_response('analyse/index.html', context, context_instance = RequestContext(request))
 
