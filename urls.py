3d1aee89fbef2c90323f83d69c9bdac177e7c3ca
Hu Kai <iamkaihu@gmail.com>
2009-10-16 00:00:49 +0800

    can load the free open chart on the web page.

--- urls.py
+++ urls.py
@@ -5,20 +5,19 @@ from django.conf.urls.defaults import *
 # admin.autodiscover()
 import settings
 
-
 urlpatterns = patterns('',
-    # Example:
-    # (r'^ianalyse/', include('ianalyse.foo.urls')),
+                       # Example:
+                       # (r'^ianalyse/', include('ianalyse.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+                       # to INSTALLED_APPS to enable admin documentation:
+                       # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    (r'^analyse/$',                          'ianalyse.analyse.views.index'),
-    (r'^analyse/(?P<build_name>\s+)/$',      'ianalyse.analyse.views.detail'),
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': '/Users/twer/Workspace/ianalyse/media', 'show_indexes': True})
-)
+                       (r'^analyse/$',                          'ianalyse.analyse.views.index'),
+                       (r'^analyse/(?P<build_name>\s+)/$',      'ianalyse.analyse.views.detail'),
+                       (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                       {'document_root': settings.MEDIA_ROOT, 'show_indexes': True})
+        )
 
 
 
