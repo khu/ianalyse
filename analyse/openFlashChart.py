888a7910efc9ebe877e6f1c8bca9ca666e3e9e6f
Hu Kai <iamkaihu@gmail.com>
2009-10-16 20:37:54 +0800

    Can generate the open flash chart.

--- analyse/openFlashChart.py
+++ analyse/openFlashChart.py
@@ -1,88 +1,78 @@
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; version 2 of the License.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
-# Author: Emanuel Fonseca
-# Email:  emdfonseca<at>gmail<dot>com
-# Date:   25 August 2008
 #
 # Author: Eugene Kin Chee Yip
-# Date:   7 Nov 2008
+# Date:   14 June 2009
 
-import	cjson
+try:
+	NO_CJSON = False
+	import	cjson
+except ImportError:
+	NO_CJSON = True
+	import json
+import	copy
 
-from	openFlashChart_elements import (title,
-										x_legend,
-										y_legend,
-										x_axis,
-										y_axis,
-										radar_axis,
-										tooltip)
+class Chart(dict):
+	# Dictionary for replacing attribute names
+	replaceKeyDictionary =	{
+		"on_show": "on-show",			"on_click": "on-click",
+		"start_angle": "start-angle",
 
+		"threeD": "3d",					"tick_height": "tick-height",
+		"grid_colour": "grid-colour",	"tick_length": "tick-length",
 
-def flashHTML(width, height, url, ofc_base_url="/flashes/", ofc_swf="OFC.swf" ):
-	return (
-		"""
-		<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
-				codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,115,0"
-				width="%(width)s" height="%(height)s" id="chart" align="middle">
-			<param name="allowScriptAccess" value="sameDomain"/>
-			<param name="movie" value="%(ofc_base_url)s%(ofc_swf)s"/>
-			<param name="FlashVars" value="data-file=%(url)s"/>
-			<param name="quality" value="high"/>
-			<param name="bgcolor" value="#FFFFFF"/>
-			<embed src="%(ofc_base_url)s%(ofc_swf)s" FlashVars="data-file=%(url)s" quality="high" bgcolor="#FFFFFF"
-				   width=%(width)s height=%(height)s name="chart" align="middle" allowScriptAccess="sameDomain"
-				   type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"/>
-		</object>
-		""") % locals()
+		"dot_style": "dot-style",		"dot_size": "dot-size",
+		"halo_size": "halo-size",
 
+		"line_style": "line-style",		"outline_colour": "outline-colour",
+		"fill_alpha": "fill-alpha",		"gradient_fill": "gradient-fill",
+	}
 
-
-class template(dict):
-	def __init__(self, title_string, style = None):
-		self['title'] = title(title_string, style)
-
-	def set_x_legend(self, legend, style):
-		self['x_legend'] = x_legend(legend, style)
-
-	def set_y_legend(self, legend, style):
-		self['y_legend'] = y_legend(legend, style)
-
-	def set_x_axis(self, stroke = None, tick_height = None, colour = None, grid_colour = None, labels = None, three_d = None, max = None, min = None, steps = None, offset = None):
-		self['x_axis'] = x_axis(stroke, tick_height, colour, grid_colour, labels, three_d, max, min, steps, offset)
-    
-	def set_y_axis(self, stroke = None, tick_length = None, colour = None, grid_colour = None, labels = None, max = None, min = None, steps = None, offset = None):
-		self['y_axis'] = y_axis(stroke, tick_length, colour, grid_colour, labels, max, min, steps, offset)
-    
-	def set_y_axis_right(self, stroke = None, tick_length = None, colour = None, grid_colour = None, labels = None, max = None, min = None, steps = None, offset = None):
-		self['y_axis_right'] = y_axis(stroke, tick_length, colour, grid_colour, labels, max, min, steps, offset)
-
-	def set_radar_axis(self, stroke = None, tick_height = None, colour = None, grid_colour = None, labels = None, max = None, min = None, steps = None, spoke_labels = None):
-		self['radar_axis'] = radar_axis(stroke, tick_height, colour, grid_colour, labels, max, min, steps, spoke_labels)
-
-
-	def set_bg_colour(self, colour):
-		self['bg_colour'] = colour
-
-	def set_tooltip(self, shadow = None, stroke = None, colour = None, bg_colour = None, title_style = None, body_style = None, behaviour = None):
-		self['tooltip'] = tooltip(shadow, stroke, colour, bg_colour, title_style, body_style, behaviour)
-
-	def add_element(self, element):
+	# Redefine to allow for nested attributes.
+	# E.g. when calling the leaf attribute, text, in chart.title.text
+	#      without previously defining the branch attribute, title.
+	def __getattribute__(self, key):
 		try:
-			self['elements'].append(element)
-		except:
-			self['elements'] = [element]
+			return dict.__getattribute__(self, key)
+		except AttributeError:
+			self.__dict__[key] = Chart()
+			return dict.__getattribute__(self, key)
 
-	def encode(self):
-		return cjson.encode(self)
-        
-        
-        
-        
+	# This copy function is called when we want to get all the attributes of the
+	# chart instance so we can pass it off to cjson to create the JSON string.
+	# Recursive trick to get leaf attributes.  Have to be careful of list types.
+	# Also, replace certain underscored keys.
+	# E.g. getting the leaf attribute, text, from the parent Chart instance where a
+	#      previous assignment was to chart.title.text
+	def __copy__(self):
+		attributes = dict()
+		for key, value in self.__dict__.items():
+			if isinstance(value, list):
+				attributes[self.replaceKey(key)] = [copy.copy(item) for item in value]
+			else:
+				attributes[self.replaceKey(key)] = copy.copy(value)
+		return attributes
+
+	# If key has an underscore, replace with a dash.
+	# Python does not allow dash in object names.
+	def replaceKey(self, key):
+		if (key in self.replaceKeyDictionary):
+			return self.replaceKeyDictionary[key]
+		else:
+			return key
+
+	# Encode the chart attributes as JSON
+	def create(self):
+		attributes = copy.copy(self)
+		if NO_CJSON:
+			return json.dumps(attributes)
+		else:
+			return cjson.encode(attributes)
+
 
--- analyse/views.py
+++ analyse/views.py
@@ -1,15 +1,65 @@
-# Create your views here.
 from django.template import Context, loader, RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-from analyse.models import *
+from analyse.models import BuildFactory, Build
+from analyse.openFlashChart import Chart
 
+def generate_chart():
+    chart = Chart()
+
+    element1 = Chart()
+    element1.values =  [19,17,19,19,17,19,17,17,14,14,17,160]
+    element1.type = "line"
+    element1.dot_style.type = "dot"
+    element1.dot_style.dot_size = 5
+    element1.dot_style.colour = "#DFC329"
+    element1.width = 4
+    element1.colour = "#DFC329"
+    element1.text = "Line 1"
+    element1.font_size = 10
+
+    element2 = Chart()
+    element2.values = [112,10,8,10,13,9,8,10,8,10,10,8]
+    element2.type = "line"
+    element2.dot_style.type = "star"
+    element2.dot_style.dot_size = 5
+    element2.width = 1
+    element2.colour = "#6363AC"
+    element2.text = "Line 2"
+    element2.font_size = 10
+
+    element3 = Chart()
+    element3.values = [7,6,2,6,6,4,3,2,5,4,2,45]
+    element3.type = "line"
+    element3.width = 1
+    element3.colour = "#5E4725"
+    element3.text = "Line 3"
+    element3.font_size = 10
+
+    # Create chart
+    chart = Chart()
+    chart.y_axis.min = 0
+    chart.y_axis.max = 20
+    chart.y_axis.font_size = 10
+    chart.title.text = "Three lines example"
+
+    #
+    # here we add our data sets to the chart:
+    #
+    chart.elements = [element1, element2, element3]
+
+    f = open("/Users/twer/Workspace/ianalyse/results/area-2.txt", 'w')
+    try:
+        f.write(chart.create())
+    finally:
+        f.close()
 
 def index(request):
     builds = BuildFactory.create_builds();
     context = Context({
-        "total" : Build.total('connectfour4'),
-        "pass_rate" : Build.pass_rate('connectfour4')
+    "total" : Build.total('connectfour4'),
+    "pass_rate" : Build.pass_rate('connectfour4')
     })
-    return render_to_response('analyse/index.html', context, context_instance = RequestContext(request))
 
+    generate_chart()
+    return render_to_response('analyse/index.html', context, context_instance = RequestContext(request))
