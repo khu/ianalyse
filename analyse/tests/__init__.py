1fcb1d8e59c3eedc6680ec3285fc730677596b4d
Hu Kai <iamkaihu@gmail.com>
2009-11-08 21:21:14 +0800

    be able to list the file on different order by parsing the log timestamp.

--- analyse/tests/__init__.py
+++ analyse/tests/__init__.py
@@ -6,6 +6,7 @@ import analyse.tests.build_total_pass_percentage_tests
 import analyse.tests.statistics_generation_tests
 import analyse.tests.successful_rate_chart
 import analyse.tests.datetimeutils_tests
+import analyse.tests.osutils_tests
 import analyse.tests.builds_tests
 import analyse.tests.config_tests
 
@@ -13,16 +14,11 @@ def suite():
     s = unittest.TestSuite()
     s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.build_tests.BuildTest))
     s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.build_factory_tests.BuildFactoryTest))
-    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(
-            analyse.tests.build_total_pass_percentage_tests.BuildTotalPassPercentageTest))
-    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(
-            analyse.tests.statistics_generation_tests.StatisticsGenerationTests))
-    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(
-            analyse.tests.successful_rate_chart.SuccessfulRateChartTests))
-    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(
-            analyse.tests.datetimeutils_tests.DatetimeUtilsTest))
-    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(
-            analyse.tests.builds_tests.BuildsTest))
-    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(
-		            analyse.tests.config_tests.ConfigTests))
+    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.build_total_pass_percentage_tests.BuildTotalPassPercentageTest))
+    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.statistics_generation_tests.StatisticsGenerationTests))
+    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.successful_rate_chart.SuccessfulRateChartTests))
+    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.datetimeutils_tests.DatetimeUtilsTest))
+    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.builds_tests.BuildsTest))
+    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.config_tests.ConfigTests))
+    s.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(analyse.tests.osutils_tests.OSUtilsTests))
     return s
