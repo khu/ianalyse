7df9e4edb2d41a833c2bac0567b89fe6fffab5b4
Hu Kai <iamkaihu@gmail.com>
2009-11-08 21:20:50 +0800

    be able to list the file on different order by parsing the log timestamp.

--- util/osutils.py
+++ util/osutils.py
@@ -1,5 +1,7 @@
 import os
-import shutil
+import shutil          
+import re                                            
+from datetime import datetime
 
 def touch(path):
     makedirs_p(os.path.dirname(path))
@@ -19,11 +21,46 @@ def write_to_file(path, content):
     try:
         f.write(content)
     finally:
-        f.close()
+        f.close() 
+                                                                   
+def list_matched_files(root, pattern):     
+    files = []
+    for file in os.listdir(root):
+        m = re.match(pattern, file)
+        if m != None:
+            files.append(file)
+    return files
+    
+def sort_by_rule(root, rule, order = 'asc'):   
+    pattern = "log([0-9]*).*.xml"
+    files = list();
+    all_files = list_matched_files(root, pattern)
+    return sorted(all_files, eval('compare_files_' + order))
+
+def __compare_files(file1, file2, order):
+    pattern = "log([0-9]*).*.xml"
+    m1 = re.match(pattern, file1)
+    date1 = datetime.strptime(m1.group(1), "%Y%m%d%H%M%S")
+    m2 = re.match(pattern, file1)
+    date2 = datetime.strptime(m2.group(1), "%Y%m%d%H%M%S")
+    if order == 'desc':
+        return  date1 > date2
+    else: 
+        return  date1 < date2
+
+def compare_files_asc(file1, file2):
+    return __compare_files(file1, file2, 'asc')
+
+def compare_files_desc(file1, file2):
+    return __compare_files(file1, file2, 'desc')                                      
+
+    
+    
 
 os.write_to_file = write_to_file
 os.touch = touch
 os.makedirs_p = makedirs_p
 os.rmdir_p = rmdir_p
+os.sort_by_rule = sort_by_rule
 
 
