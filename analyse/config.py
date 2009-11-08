79fca2883b65d0bbbdfa1970c5f737510245ae51
Hu Kai <iamkaihu@gmail.com>
2009-11-08 15:22:36 +0800

    be able to read the logdir.

--- analyse/config.py
+++ analyse/config.py
@@ -1,10 +1,19 @@
 import os                                                  
 from django.conf import settings
+import ConfigParser
 
 class Config:
-    def __init__(self, config_file = os.path.abspath(os.path.join(settings.PROJECT_DIR, 'ianalyse.cfg'))):
+    def __init__(self, config_file = os.path.join(settings.PROJECT_DIR, 'ianalyse.cfg')):
         self.config_file = config_file
     
     def abspath(self):
-        return self.config_file
+        return os.path.abspath(self.config_file)                                        
+    
+    def exist(self):
+        return os.path.exists(self.abspath())
+    
+    def logdir(self): 
+        config = ConfigParser.ConfigParser()
+        config.read(self.abspath())
+        return config.get('Basic', 'logdir', 0)
     
--- analyse/tests/config_tests.py
+++ analyse/tests/config_tests.py
@@ -6,18 +6,24 @@ from analyse.config import Config
 
 class ConfigTests(TestCase):   
     def setUp(self):
-        self.config_file = os.path.abspath(os.path.join(settings.PROJECT_DIR, 'ianalyse.cfg'))
+        self.config = Config(os.path.abspath(os.path.join(settings.PROJECT_DIR, 'analyse/tests/fixtures/config/ianalyse.cfg')))
+        
+    def testShouldReturnTheAbsolutePathOfTheDefaultConfigFile(self):
+        expected = os.path.abspath(os.path.join(settings.PROJECT_DIR, 'ianalyse.cfg'))
+        config = Config()
+        self.assertEquals(expected, config.abspath())
 
-    def testShouldReturnTheAbsolutePathOfTheConfigFile(self):
-	    config = Config()
-	    self.assertEquals(self.config_file, config.abspath())
-	                                              
+    def testShouldReturnSpecificConfigFile(self):
+        expected = os.path.abspath(os.path.join(settings.PROJECT_DIR, 'analyse/tests/fixtures/config/ianalyse.cfg'))
+        config = Config(expected)
+        self.assertEquals(expected, config.abspath())
     
-    def testShouldThrowExceptionWhenConfigFileIsMissing(self):
-        pass
+    def testShouldReturnFalseWhenConfigFileIsMissing(self):
+        not_exist_file = os.path.abspath(os.path.join(settings.PROJECT_DIR, 'analyse/tests/fixtures/config/not.exist.cfg'))
+        self.assertEquals(False, Config(not_exist_file).exist())
 	  
     def testShouldReadTheLogDir(self):
-	    pass
+        self.assertEquals('/var/logs', self.config.logdir())
 	
     def testShouldReturnNDaysIfDefined(self):
 	    pass
